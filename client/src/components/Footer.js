bf3ca3cb0c304c408d3295f3c912b3394428259a
mindWhisperer <148146743+mindWhisperer@users.noreply.github.com>
2025-04-15 17:38:58 +0200

    paticka

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/index.css"
+
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <p className="footer-text">Anonymizacia udajov</p>
+        </footer>
+    );
+};
+
+export default Footer;
